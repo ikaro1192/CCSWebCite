f239ed70a72614321764a22798f376ed6047d5b8
ikaro <aaxa2108@chiba-u.jp>
2013-02-16 14:19:59 +0900

    ちょっと綺麗にした

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -7,31 +7,6 @@ import cgi
 import os
 from data_module import *
 
-
-def addTopics():
-	Data = TopicsData(u"タイトル",u"中身")
-	print Data.encodeJson() 
-	con=sqlite3.connect('./dbfile.db')
-	cur=con.cursor()
-	try:
-		cur.execute("""create table Topics(
-						id INTEGER PRIMARY KEY,
-						title text,
-						date text,
-						body text);"""
-					)
-
-		cur.execute(u"""insert into Topics ( title, date, body)
-						values  ("hogefoobar","2012-11-17", "this is text");""")
-	except:
-		print "hoge"
-		pass
-
-	con.commit()
-
-	return
-
-
 def getTopics():
 	print "Content-type: text/javascript;charset=utf-8\n"
 
