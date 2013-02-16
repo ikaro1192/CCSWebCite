41f6cfd148a5b7b6920139de4be48a23f3d642e6
ikaro <aaxa2108@chiba-u.jp>
2013-02-16 13:57:10 +0900

    アップロードと分離

---
+++ cgi-bin/data_module.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import locale 
+import sqlite3
+import cgi
+import os
+
+class Request(object):
+	def __init__(self, environ=os.environ):
+		self.form__=cgi.FieldStorage()
+		self.environ__=environ
+
+	def getreq(self,key):
+		if self.form__.has_key(key) == False :
+			raise KeyError
+		return self.form__[key].value
+		
+
+class TopicsData(object):
+	def __init__(self, Title, Body, Date = datetime.datetime.today() ):
+		#Parameter sequence is't elegant...
+		self.__title = Title
+		self.__body = Body
+		self.__date = Date
+		return
+
+	def encodeJson(self):
+		"""TopicsData encode to Json Data"""
+		Param = {"title":self.__title, "date":self.__date.strftime("%Y-%m-%d"), "body":self.__body}
+		return u"{'title':'%(title)s', 'date':'%(date)s', 'body':'%(body)s'}" % Param
+
+
+
--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -5,57 +5,59 @@ import locale
 import sqlite3
 import cgi
 import os
-
-class Request(object):
-	def __init__(self, environ=os.environ):
-		self.form__=cgi.FieldStorage()
-		self.environ__=environ
-
-	def getreq(self,key):
-		if self.form__.has_key(key) == False :
-			raise KeyError
-		return self.form[key].value
-		
-
-class TopicsData(object):
-	def __init__(self, Title, Body, Date = datetime.datetime.today() ):
-		self.__title = Title
-		self.__body = Body
-		self.__date = Date
-		return
-
-	def encodeJson(self):
-		Param = {"title":self.__title, "date":self.__date.strftime("%Y-%m-%d"), "body":self.__body}
-		return u'{"title":"%(title)s", "date":"%(date)s", "body":"%(body)s"}' % Param
-
-		return 
+from data_module import *
 
 
-if __name__ == '__main__':
+def addTopics():
 	Data = TopicsData(u"タイトル",u"中身")
-
-	print "Content-type: application/json;charset=utf-8\n"
 	print Data.encodeJson() 
-
-	try:
-		req=Request()
-		req.getreq("foo")
-	except KeyError:
-		print "bar"
-
-
 	con=sqlite3.connect('./dbfile.db')
 	cur=con.cursor()
 	try:
 		cur.execute("""create table Topics(
-						id serial,
+						id INTEGER PRIMARY KEY,
 						title text,
 						date text,
 						body text);"""
 					)
 
-		cur.execute(u"""insert into table values  (id serial, title text, date text, body text);""")
+		cur.execute(u"""insert into Topics ( title, date, body)
+						values  ("hogefoobar","2012-11-17", "this is text");""")
 	except:
 		print "hoge"
 		pass
+
 	con.commit()
+
+	return
+
+
+def getTopics():
+	print "Content-type: text/javascript;charset=utf-8\n"
+
+	try:
+		req=Request()
+		TopicID=int(req.getreq("id"))
+	except KeyError:
+		TopicID = 1
+		pass
+
+	con=sqlite3.connect('./dbfile.db')
+	cur=con.cursor()
+
+	#Select Topics Data
+	cur.execute(""" select title, date, body from Topics where id = %d;"""%TopicID)
+	result = cur.fetchall()		
+	if (result != []):
+		for res in result:
+			DateData=TopicsData(res[0], res[2], datetime.datetime.strptime(res[1],"%Y-%m-%d"))
+			print "callback("+DateData.encodeJson()+")"
+	else:
+		#return empty Json
+		print "callback({})"
+
+
+if __name__ == '__main__':
+	getTopics()
+
+
